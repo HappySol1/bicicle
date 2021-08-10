56fca99e2db831102dfa646c901bef9c805df3f6
HappySol1 <88682993+HappySol1@users.noreply.github.com>
2021-08-10 23:22:12 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	function ibg(){
+	$.each($('.ibg'), function(index, val) {
+	if($(this).find('img').length>0){
+	$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+	}
+	});
+	}
+
+	ibg();
+});
+
+$('.your-class').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  //autoplay: true,
+  autoplaySpeed: 2000,
+});
